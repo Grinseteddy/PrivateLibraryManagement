4df03d54e52b15bd8382d928bc65bdcedf504c81
annegretjunker <annegret.junker@allianz.de>
2023-09-15 15:34:03 +0200

    Initial creation

---
+++ src/main/java/com/grinseteddy/privatelibrarymanagement/PrivateLibraryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.grinseteddy.privatelibrarymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateLibraryManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrivateLibraryManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grinseteddy/privatelibrarymanagement/PrivateLibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grinseteddy.privatelibrarymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateLibraryManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
